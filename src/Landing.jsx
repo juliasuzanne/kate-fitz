b5863adcbbd195f3f7ac818d546f2e3a760f49c5
Julia Grimes <julia.s.grimes@gmail.com>
2023-07-13 01:16:06 -0400

    Added lazy to landing page div, not sure if it will help

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useEffect } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export function Landing() {
   const ref = useRef(null);
@@ -11,14 +12,12 @@ export function Landing() {
   useEffect(handleOpenPage);
 
   return (
-    <div className="background">
-      <div />
+    <div loading="lazy" className="background">
+      <div ref={ref} id="scrollto"></div>
 
       <a href="/home" className="heading-enter">
         enter
       </a>
-
-      <div ref={ref} id="scrollto"></div>
     </div>
   );
 }
