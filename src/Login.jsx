48892199ada91a171c47df0327635a7dde9c1adf
Julia Grimes <julia.s.grimes@gmail.com>
2023-07-15 19:17:07 -0400

    Fixed login issue

--- src/Login.jsx
+++ src/Login.jsx
@@ -11,22 +11,39 @@ export function Login() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setErrors([]);
     const params = new FormData(event.target);
+    setErrors([]);
     axios
       .post("https://kate.fly.dev/sessions", params)
       .then((response) => {
         console.log(response.data);
-        defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
         localStorage.setItem("jwt", response.data.jwt);
         event.target.reset();
-        window.location.href = "/changemachine"; // Change this to hide a modal, redirect to a specific page, etc.
+        window.location.href = "/changemachine";
       })
-      .catch((error) => {
-        console.log(error.response);
+      .catch((errors) => {
+        console.log(errors.response);
         setErrors(["Invalid email or password"]);
       });
   };
+
+  // const handleSignupSubmit = (event) => {
+  //   event.preventDefault();
+  //   setErrors([]);
+  //   const params = new FormData(event.target);
+  //   axios
+  //     .post("https://kate.fly.dev/users", params)
+  //     .then((response) => {
+  //       console.log(response);
+  //       event.target.reset();
+  //       window.location.href = "/changemachine";
+  //     })
+  //     .catch((errors) => {
+  //       console.log(errors.response.data.errors);
+  //       setErrors(errors.response.data.errors);
+  //     });
+  // };
 
   return (
     <div id="login">
@@ -47,6 +64,27 @@ export function Login() {
           Login
         </button>
       </form>
+
+      {/* <h1 className="heading-login">Signup</h1>
+      <ul className="handwriting">
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
+      <form className="myform" onSubmit={handleSignupSubmit}>
+        <div className="handwriting">
+          Name: <input name="name" type="name" />
+        </div>
+        <div className="handwriting">
+          Password: <input name="password" type="password" />
+        </div>
+        <div className="handwriting">
+          Password Confirmation: <input name="password_confirmation" type="password" />
+        </div>
+        <button id="button-login" className="handwriting" type="submit">
+          Signup
+        </button>
+      </form> */}
     </div>
   );
 }
